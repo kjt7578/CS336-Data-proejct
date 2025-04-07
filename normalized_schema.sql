557e756f2c442674053f72bde41bd1cc98554b88
JT <56941721+kjt7578@users.noreply.github.com>
2025-04-07 17:50:13 -0400

    Update normalized_schema.sql

--- normalized_schema.sql
+++ normalized_schema.sql
@@ -1,1 +1,17 @@
+-- Creating tables 
+CREATE TABLE Loan_Type (
+  loan_type INT PRIMARY KEY,
+  loan_type_name TEXT
+);
 
+
+
+
+
+
+
+-- Inserting data
+INSERT INTO Loan_Type
+SELECT DISTINCT loan_type, loan_type_name
+FROM preliminary
+WHERE loan_type IS NOT NULL;
