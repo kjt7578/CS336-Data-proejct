77317e06bd977d1d65f64fae60bc24dbad5a27e2
JT <56941721+kjt7578@users.noreply.github.com>
2025-04-09 13:36:55 -0400

    Create error.sql

---
+++ error.sql
@@ -0,0 +1,8 @@
+INSERT INTO Application (id, as_of_year, respondent_id, agency_code, loan_type, property_type, loan_purpose, owner_occupancy, loan_amount_000s, preapproval, action_taken, location_id, applicant_ethnicity, co_applicant_ethnicity, applicant_sex, co_applicant_sex, applicant_income_000s, purchaser_type, denial_reason_1, denial_reason_2, denial_reason_3, rate_spread, hoepa_status, lien_status, sequence_number, application_date_indicator)
+VALUES (99999999, 2017, 'fake_id', 999, 1, 1, 1, 1, 100, 1, 1, 1, 1, 1, 1, 1, 100, 1, NULL, NULL, NULL, NULL, 1, 1, '123456', 'A');
+
+INSERT INTO Application (id, as_of_year, respondent_id, agency_code, loan_type, property_type, loan_purpose, owner_occupancy, loan_amount_000s, preapproval, action_taken, location_id, applicant_ethnicity, co_applicant_ethnicity, applicant_sex, co_applicant_sex, applicant_income_000s, purchaser_type, denial_reason_1, denial_reason_2, denial_reason_3, rate_spread, hoepa_status, lien_status, sequence_number, application_date_indicator)
+VALUES (99999998, NULL, 'responder', 1, 1, 1, 1, 1, 100, 1, 1, 1, 1, 1, 1, 1, 100, 1, NULL, NULL, NULL, NULL, 1, 1, '123456', 'A');
+
+INSERT INTO Applicant_Race (application_id, race_number, race_code)
+VALUES (1, 6, 1);
